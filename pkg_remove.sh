d6a115a9a35e34c77a2bbae5311e785f0d9759ac
Naruto Windy <narutowindy@users.noreply.github.com>
2017-07-09 11:18:50 +0530

    Add files via upload

---
+++ pkg_remove.sh
@@ -0,0 +1,1 @@
+opkg remove kmod-ppp kmod-pppoe kmod-pppox kmod-usb-core kmod-usb-ohci kmod-usb2 --force-depends
---
+++ wifi.sh
@@ -0,0 +1,1 @@
+uci set wireless.@wifi-device[0].disabled=; uci commit wireless; wifi
